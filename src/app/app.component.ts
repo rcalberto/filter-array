36349a83b5faf22803a4e02a4f5e297b3cab3318
binario <rcali.est@gmail.com>
2023-03-07 14:24:24 -0500

    'v1'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Observable, of } from "rxjs";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'practicaFilter';
+  title = "practicaFilter";
+  filterModel: string = "";
+  personas$: Observable<any>;
+
+  personas: string[] = [
+    "Iron Man",
+    "Spiderman",
+    "Thor",
+    "Hulk",
+    "Black Widow",
+    "Hawk Eye",
+  ];
+  constructor() {
+    this.personas$ = of(this.personas);
+  }
+
+  filterList(term: string) {
+    if (term.length < 2) {
+      this.personas$ = of(this.personas);
+    } else {
+      this.personas$ = of(
+        this.personas.filter((resp) =>
+          resp.toLowerCase().includes(term.toLowerCase())
+        )
+      );
+    }
+  }
 }
